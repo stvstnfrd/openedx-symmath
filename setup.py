54712f37f60ff1a6d67d420945c8e4457fb9f491
stv <stv@stanford.edu>
2019-10-23 12:42:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from __future__ import absolute_import
 from setuptools import setup
 
+
 setup(
     name="symmath",
     version="0.3",
     packages=["symmath"],
     install_requires=[
+        'lxml',
+        'six',
         "sympy",
+    ],
+    test_suite='symmath.tests',
+    tests_require=[
+        'coverage',
     ],
 )
